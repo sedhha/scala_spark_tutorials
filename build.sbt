23215a319ff410f9eaa691877d4303cb98e82d58
sedhha <activity.schoolsh2@gmail.com>
2024-10-19 16:10:18 +0530

    Added end chapter files

--- build.sbt
+++ build.sbt
@@ -8,20 +8,13 @@ lazy val root = (project in file("."))
     libraryDependencies ++= Seq(
       "org.apache.spark" %% "spark-core" % "3.4.2",
       "org.apache.spark" %% "spark-sql" % "3.4.2"
-    ),
+      ),
 
     dependencyOverrides += "org.scala-lang" % "scala-library" % scalaVersion.value,
 
     fork := true,
 
     javaOptions ++= Seq(
-      "-Dlog4j.configuration=file:./src/main/scala/resources/log4j.properties",
-      "--add-opens=java.base/java.nio=ALL-UNNAMED",                              // Allow access to java.nio
-      "--add-opens=java.base/sun.nio.ch=ALL-UNNAMED",                            // Allow access to sun.nio.ch
-      "--add-opens=java.base/java.util=ALL-UNNAMED",                             // Allow access to java.util
-      "--add-opens=java.base/java.lang=ALL-UNNAMED",                             // Allow access to java.lang
-      "--add-opens=java.base/java.util.concurrent=ALL-UNNAMED",                  // Allow access to java.util.concurrent
-      "--add-opens=java.base/java.net=ALL-UNNAMED",                              // Allow access to java.net (for URI.scheme)
-      "--illegal-access=permit"
+      "-Dlog4j.configuration=file:./src/main/scala/resources/log4j.properties"
     )
   )
--- src/main/scala/Chapter2.scala
+++ src/main/scala/Chapter2.scala
@@ -1,26 +1,62 @@
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col,explode,split, lower}
 
 object Chapter2 extends App {
   private val spark = SparkSession.builder()
     .appName("Chapter2")
     .master("local[*]")
     .getOrCreate()
-  import spark.implicits._
-  // Reading text file with RDD ->
-  private val rddText = spark.sparkContext.textFile("./src/assets/sample.txt")
-  println(s"This is the RDD version of the text file")
-  rddText.collect().foreach(println)
-  // Reading text file with DataFrame ->
-  println(s"This is the DataFrame version of the text file")
-  private val dfText = spark.read.text("./src/assets/sample.txt")
+  
+  /*
+    How to read a text file in Spark
+    How to convert RDD to DataFrame and vice versa
+    Line Count of given text file
+    Word Count of given text file
+  */
+  
+  // Through RDDs and DataFrames
+  private val pathToTextFile = "./src/assets/sample.txt"
+
+  // Through RDDs
+  private val rddText = spark.sparkContext.textFile(pathToTextFile)
+  // rddText.collect().foreach(println)
+  // println(s"Line Count of the given file - ${rddText.collect().length}")
+  
+  // Thorugh DF
+  private val dfText = spark.read.text(pathToTextFile)
   dfText.show()
-  // Converting rdd to dataframe and vice versa
-  private val rddToDf = rddText.toDF("line")
-  println(s"This is the DataFrame version of the RDD")
-  rddToDf.show()
-  private val dfTordd = dfText.rdd
-  println(s"This is the RDD version of the DataFrame")
-  dfTordd.collect().foreach(println)
+
+  // Conversions
+
+  // Convert to DF from RDD
+  // println("Convert to DF from RDD")
+  import spark.implicits._
+  private val dfFromRDD = rddText.toDF("something_else")
+  // dfFromRDD.show()
+
+  // Convert to RDD from DF
+  // println("Convert to RDD from DF")
+  private val rddFromDF = dfText.rdd
+  // rddFromDF.collect().foreach(println)
+  
+  // Word Count
+  // println("Word Count of the given file")
+  private val numberOfWords = rddText
+  .flatMap(line => line.split(" "))
+  .map(word => (word.toLowerCase, 1))
+  .reduceByKey((value1,value2) => value1 + value2)
+  // numberOfWords.collect().foreach(println)
+  
+  // Do this with dataframe
+
+  private val dfWordCounts = dfText
+                              .select(
+                                explode(split(lower(col("value")), " ")).as("word")
+                              )
+
+  private val dfAggregation = dfWordCounts.groupBy("word").count()
+  println("WOrd count with dataframe")
+  dfAggregation.show()
 
 
   spark.stop()
